0a5fae408acfcec0c6d966dfa6871d27878b6234
Anshul Saha <anshulsaha15@gmail.com>
2021-08-11 09:15:00 +0530

    react starter

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Header from './components/ui/Header'
-import CharacterGrid from './components/characters/CharacterGrid'
-import Search from './components/ui/Search'
+import React from 'react';
+import Title from './comps/Title'
 import './App.css'
-import axios from 'axios';
 
-const  App = () => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [query, setQuery] = useState('');
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      setIsLoading(true)
-      const result = await axios(`https://breakingbadapi.com/api/characters?name=${query}`)
-      setItems(result.data)
-      setIsLoading(false)
-    }
-
-    fetchItems();
-  }, [query])
-
+const App = () => {
   return (
-    <div className="container">
-      <Header />
-      <Search getQuery={(q) => setQuery(q)} />
-      <CharacterGrid isLoading={isLoading} items={items} />
+    <div className="App">
+      <Title />
     </div>
   );
 }
---
+++ src/comps/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+export default Title
+
+// rafce is the shortcut to create function based components
